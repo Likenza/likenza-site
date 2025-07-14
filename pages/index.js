ac0284820b915364f38ebfa242ff366b4b64f8b1
Likenza <admin@likenza.com>
2025-07-14 11:58:15 +0200

    Update index.js
    Sections 1 - 5

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,17 @@
+import Section1_Hero from '../components/Section1_Hero';
+import Section2_Why from '../components/Section2_Why';
+import Section3_Pillars from '../components/Section3_Pillars';
+import Section4_Universes from '../components/Section4_Universes';
+import Section5Belief from '../components/Section5Belief';
+
 export default function Home() {
   return (
     <main>
-      <h1>Welcome to Likenza</h1>
-      <p>This is a placeholder for the full landing page content.</p>
+      <Section1_Hero />
+      <Section2_Why />
+      <Section3_Pillars />
+      <Section4_Universes />
+      <Section5Belief />
     </main>
   );
 }
